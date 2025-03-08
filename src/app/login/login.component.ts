feadd26f8f0bd625447c90be50e9781a003f98c9
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-03-08 16:45:53 +0200

    creating register & login forms with validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private router: Router){}
+
+  formValue = {
+    email: '',
+    password: ''
+  } 
+
+  handleLogin(form: NgForm) {
+    if (form.invalid) {
+      form.form.markAllAsTouched(); // Highlight validation errors
+      return;
+    }
+    
+    this.router.navigate(['/']);
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, ValidatorFn, ValidationErrors  } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  
+  registerForm: FormGroup;
 
+  
+  constructor(private fb: FormBuilder, private router: Router){
+    this.registerForm = this.fb.group({
+      name: ['',[Validators.required, Validators.minLength(2)]],
+      email:['', [Validators.email, Validators.required]],
+      username: ['', [Validators.required, Validators.pattern('^\\S+$')]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required, this.confirmPassValidator()]]
+    }) 
+
+  }
+
+  get formControls(){
+    return this.registerForm.controls;
+  }
+
+  confirmPassValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = this.registerForm?.get('password')?.value;
+      const confirmPassword = control.value;
+      
+      return password === confirmPassword ? null : { passwordMismatch: true };
+    };
+  }
+
+  handleRegister() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched(); // Highlight errors
+      return;
+    }
+    
+    console.log("Registration Successful", this.registerForm.value);
+    this.router.navigate(['/']); // Redirect to Home
+  }
 }
